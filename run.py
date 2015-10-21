d3639c4136f00fc5a89c85bc86ffcd7b6a2a8cff
Ed Singleton <singletoned@gmail.com>
2015-10-21 22:17:26 +0100

    Whitespace fix

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import shipping
 import data
 
+
 def shortest_routes():
     print "Shortest BA-L",
     print data.shipping_map.find_path(
@@ -10,6 +11,7 @@ def shortest_routes():
     print "Shortest NY-NY",
     print data.shipping_map.find_path(
         data.NEW_YORK, data.NEW_YORK)
+
 
 if __name__ == '__main__':
     shortest_routes()
