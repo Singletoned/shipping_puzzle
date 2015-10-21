d28410790f758077ae89803933cb909a59f9412e
Ed Singleton <singletoned@gmail.com>
2015-10-21 23:09:58 +0100

    Fix whitespace

--- answers.py
+++ answers.py
@@ -30,6 +30,7 @@ def shortest_routes():
     print "Shortest BA-L",
     print data.shipping_map.find_path(
         data.BUENOS_AIRES, data.LIVERPOOL)
+
     print "Shortest NY-NY",
     print data.shipping_map.find_path(
         data.NEW_YORK, data.NEW_YORK)
