6e679511faa43e51776e5b9e9b2face8df669cbf
Ed Singleton <singletoned@gmail.com>
2015-10-21 18:26:03 +0100

    Use unittest asserts

--- test_shipping.py
+++ test_shipping.py
@@ -8,9 +8,10 @@ import shipping
 class TestRoute(unittest.TestCase):
     def test_init(self):
         r = shipping.Route("foo", "bar", 5)
-        assert r.start == "foo"
-        assert r.end == "bar"
-        assert r.days == 5
+        self.assertEqual(r.start, "foo")
+        self.assertEqual(r.end, "bar")
+        self.assertEqual(r.days, 5)
+
 
 
 if __name__ == '__main__':
