eec169af71925198708461e1d60863426d551e02
Ed Singleton <singletoned@gmail.com>
2015-10-21 22:17:17 +0100

    Add some prints to run

--- run.py
+++ run.py
@@ -4,8 +4,10 @@ import shipping
 import data
 
 def shortest_routes():
+    print "Shortest BA-L",
     print data.shipping_map.find_path(
         data.BUENOS_AIRES, data.LIVERPOOL)
+    print "Shortest NY-NY",
     print data.shipping_map.find_path(
         data.NEW_YORK, data.NEW_YORK)
 
